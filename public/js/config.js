dceb314cf3b5ae357dde35fd4576fce603966704
Kunal-Barve <kunal.barve1201@gmail.com>
2025-05-10 09:53:59 +0530

    Delete public/js/config.js
    Removed the config file

--- public/js/config.js
+++
@@ -1,9 +0,0 @@
-// Configuration for the application
-window.appConfig = {
-    // For development, hardcode the API key here
-    // For production, this will be replaced during the build process
-    googleMapsApiKey: 'AIzaSyDAe78oRpFzdmWyjMOzVDAZY4HaM_eK2ng'
-};
-
-// Log that config is loaded
-console.log('Configuration loaded');
